ed539e5ba4de31c76fb40d573447f907982ec77d
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-11-04 16:49:51 +0700

    Add MainScreen

---
+++ app/src/main/java/com/rujirakongsomran/jc_bottomnavigation/MainScreen.kt
@@ -0,0 +1,16 @@
+package com.rujirakongsomran.jc_bottomnavigation
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = {},
+    ) {
+
+    }
+}
