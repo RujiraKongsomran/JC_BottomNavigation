f5369c95b63024ae3f11b49938cd60ae5c604e35
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-11-04 17:09:17 +0700

    Create BottomBar

--- app/src/main/java/com/rujirakongsomran/jc_bottomnavigation/MainScreen.kt
+++ app/src/main/java/com/rujirakongsomran/jc_bottomnavigation/MainScreen.kt
@@ -1,16 +1,35 @@
 package com.rujirakongsomran.jc_bottomnavigation
 
+import androidx.compose.material.BottomNavigation
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
-
     Scaffold(
         bottomBar = {},
     ) {
         BottomNavGraph(navController = navController)
+    }
+}
+
+@Composable
+fun BottomBar(navController: NavHostController) {
+    val screens = listOf(
+        BottomBarScreen.Home,
+        BottomBarScreen.Profile,
+        BottomBarScreen.Settings
+    )
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    BottomNavigation {
+
     }
 }
